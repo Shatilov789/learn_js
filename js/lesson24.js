17d1eccbda88e8da6ccb84560fb83d17c21dfb10
Shatilov789 <81092855+Shatilov789@users.noreply.github.com>
2024-09-08 00:04:47 +0300

    Add lesson24 & update html

---
+++ js/lesson24.js
@@ -0,0 +1,33 @@
+                    // N1
+// Перепишіть функцію, використовуючи '?' або '||'
+// function checkAge(age) {
+//     return age > 18 ? "Join" : "Your perants know?";
+// }
+
+// console.log(checkAge(20));
+
+                    // N2
+
+// Напишіть функцію min(a, b), яка повертає менше з двох чисел a та b
+
+// let findingSmallerNumber = (one, two) => {
+//     return one > two ? two : one;
+// }
+
+// console.log(findingSmallerNumber(23 , 21))
+
+                    // N3
+
+// Перепишіть з використанням стрілкових функцій
+// Замініть Функціональні Вирази на стрілкові функції у коді нижче
+
+// let accept = () => { alert("Ви погодились."); };
+// let cancel = () => { alert("Ви скасували виконання.") };
+
+// let ask = (question, yes, no) => {
+//     if (confirm(question)) yes();
+//     else no();
+// }
+
+// ask("Ви згодні?", accept, cancel
+// );
